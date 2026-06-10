6f701753dcd467a92702ee4cb3b09afef6c9f3c0
Adrian Jarvis <adrian.r.jarvis@gmail.com>
2026-06-10 21:57:23 +1200

    Split the disks into a 15GB boot disk and a 100G data disk for immich. Start adding the set up of a load balancer for TLS termination.

--- main.tf
+++ main.tf
@@ -82,23 +82,38 @@ resource "openstack_networking_floatingip_v2" "floatip" {
 
 resource "openstack_blockstorage_volume_v3" "bootdisk" {
     name = "immich-root-disk"
-    size = 80
+    size = 15
     volume_type = "b1.sr-r3-nvme-1000"
     image_id = data.openstack_images_image_v2.ubuntu.id
 }
 
+resource "openstack_blockstorage_volume_v3" "appdisk" {
+    name = "immich-app-disk"
+    size = 100
+    volume_type = "b1.sr-r3-nvme-1000"
+}
+
 resource "openstack_compute_instance_v2" "server" {
   name            = "immich-server"
-  flavor_id       = data.openstack_compute_flavor_v2.flavor.id
+  flavor_name       = "c1.c4r8"
   key_pair        = openstack_compute_keypair_v2.keypair.name
   security_groups = [openstack_networking_secgroup_v2.security_group.name]
   user_data       = templatefile("cloud-init.tftbl", { db_password = var.db_password } )
 
   block_device {
-    delete_on_termination = false
+    delete_on_termination = true
     uuid = openstack_blockstorage_volume_v3.bootdisk.id
     source_type = "volume"
     destination_type = "volume"
+    boot_index = 0
+  }
+
+  block_device {
+    delete_on_termination = false
+    uuid = openstack_blockstorage_volume_v3.appdisk.id
+    source_type = "volume"
+    destination_type = "volume"
+    boot_index = -1
   }
 
   network {
@@ -114,6 +129,33 @@ data "openstack_networking_port_v2" "port" {
 resource "openstack_networking_floatingip_associate_v2" "fip_vm" {
   floating_ip = openstack_networking_floatingip_v2.floatip.address
   port_id     = data.openstack_networking_port_v2.port.id
+}
+
+resource "openstack_lb_loadbalancer_v2" "lb" {
+  vip_subnet_id = openstack_networking_subnet_v2.subnet.id
+  name = "immich-lb"
+  security_group_ids = [ openstack_networking_secgroup_v2.security_group.id ]
+}
+
+resource "openstack_lb_listener_v2" "listener_1" {
+  name            = "immich-listener"
+  protocol        = "HTTP"
+  protocol_port   = 80
+  loadbalancer_id = openstack_lb_loadbalancer_v2.lb.id
+}
+
+resource "openstack_lb_pool_v2" "pool_1" {
+  name            = "immich-pool"
+  protocol        = "HTTP"
+  lb_method       = "ROUND_ROBIN"
+  loadbalancer_id = openstack_lb_loadbalancer_v2.lb.id
+}
+
+resource "openstack_lb_member_v2" "member_1" {
+  name = "immich-member"
+  address = openstack_compute_instance_v2.server.access_ip_v4
+  pool_id = openstack_lb_pool_v2.pool_1.id
+  protocol_port = 2283
 }
 
 output "server_ip" {
